86a9f28ba05acc5f87dd4771ac75c8e8b281c28b
lizhijun <lzj@lzjwlt.cn>
2018-11-28 16:30:06 +0800

    fix: ignore https cert

--- bin/router-run-rdc-mips.sh
+++ bin/router-run-rdc-mips.sh
@@ -3,8 +3,7 @@
 # one step to run
 # sh -c "$(curl -fsSLk https://raw.githubusercontent.com/lzjwlt/remoteboot/master/bin/router-run-rdc-mips.sh)"
 
-set FILEURL="https://raw.githubusercontent.com/lzjwlt/remoteboot/master/bin/rdc_linux_mipsle"
 chdir /sys/fs/cgroup
-curl -o /sys/fs/cgroup/rdc $FILEURL
+curl -o /sys/fs/cgroup/rdc https://raw.githubusercontent.com/lzjwlt/remoteboot/master/bin/rdc_linux_mipsle -k
 chmod a+x /sys/fs/cgroup/rdc
 ./rdc lzjwlt.cn 30000

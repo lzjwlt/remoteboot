d6b171d11ce005e472917075faad1b67c51da590
lizhijun <lzj@lzjwlt.cn>
2018-11-28 16:51:17 +0800

    add: loop

--- bin/router-run-rdc-mips.sh
+++ bin/router-run-rdc-mips.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 # one step to run
-# sh -c "$(curl -fsSLk https://raw.githubusercontent.com/lzjwlt/remoteboot/master/bin/router-run-rdc-mips.sh)"
+# setsid sh -c "$(curl -fsSLk https://raw.githubusercontent.com/lzjwlt/remoteboot/master/bin/router-run-rdc-mips.sh)"
 
 chdir /sys/fs/cgroup
 curl -o /sys/fs/cgroup/rdc https://raw.githubusercontent.com/lzjwlt/remoteboot/master/bin/rdc_linux_mipsle -k
 chmod a+x /sys/fs/cgroup/rdc
-./rdc lzjwlt.cn 30000
+
+while true
+do
+    ./rdc lzjwlt.cn 30000
+    sleep 20
+done
